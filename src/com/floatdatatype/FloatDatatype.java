1ec0cb4c17a554633480eacd1df116a2996a8dec
akashvelmurugan <akashvelmurugan125@gmail.com>
2022-02-23 10:25:21 +0530

    FloatAndDoubleEqualOrNotProgram

--- src/com/floatdatatype/FloatDatatype.java
+++ src/com/floatdatatype/FloatDatatype.java
@@ -2,6 +2,8 @@ package com.floatdatatype;
 
 public class FloatDatatype {
 	public static void main(String[] args) {
+		
+		//binarydigit 110.1
 		float a=6.5f;
 		double b=6.5d;
 		if (a==b) {
@@ -9,7 +11,7 @@ public class FloatDatatype {
 		} else {
 			System.out.println("'a' and 'b' are not equal .......");
 		}
-
+		//binarydigit 110.0001100110011001101
 		a=6.1f;
 		b=6.1d;
 		if (a==b) {
@@ -17,6 +19,7 @@ public class FloatDatatype {
 		} else {
 			System.out.println("'a' and 'b' are not equal .......");
 		}
+		//binarydigit 110
 		a=6.0f;
 		b=6.0d;
 		if (a==b) {
